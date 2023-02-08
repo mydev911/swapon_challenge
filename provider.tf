a40eeb7d3f1f15803ad49307ac7d9d548d8b69c8
mydev911 <mydev911@gmail.com>
2023-02-08 07:32:11 -0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "1.3.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "awsstatic2023"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "us-east-1"
+}
